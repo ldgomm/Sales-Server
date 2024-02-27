fcd25c737b5f4ae1f0521d582b32da28f879002a
ldgomm <ruiz211@protonmail.com>
2024-02-27 11:56:23 -0500

    Working on models

---
+++ src/main/kotlin/com/premierdarkcoffee/plugins/sales_feature/data/model/sales/Size.kt
@@ -0,0 +1,6 @@
+package com.premierdarkcoffee.plugins.sales_feature.data.model.sales
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Size(val width: Double, val height: Double, val deep: Double? = null, val unit: String)
